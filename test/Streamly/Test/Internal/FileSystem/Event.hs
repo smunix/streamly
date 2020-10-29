94794d6ce786859a4da34a488eca2ce895d0d978
Ranjeet Ranjan <ranjeetr@composewell.com>
2020-10-29 14:59:08 +0530

    Add some debug print coz it fails only in automation

--- test/Streamly/Test/Internal/FileSystem/Event.hs
+++ test/Streamly/Test/Internal/FileSystem/Event.hs
@@ -58,7 +58,7 @@ baseList =
     , "Oct23_1073741840_Dir"
     , "Oct23/Oct24_1073741856_Dir"
     , "Oct23/Oct24_1073741825_Dir"
-    , "Oct23/Oct24_1073741840_Dir"
+    , "Oct23/Oct24_1073741840_DirX"
     , "Oct23/Oct24/Oct25_1073741856_Dir"
     , "Oct23/Oct24/Oct25_1073741825_Dir"
     , "Oct23/Oct24/Oct25_1073741840_Dir"
@@ -209,6 +209,7 @@ validate :: IO [Char]
 validate = do
     let baseSet = Set.fromList baseList
     resultSet <- processResult
+    mapM_ (\x -> if (Set.member x resultSet) then  print x else print (x ++ "....Missed" ) ) baseList    
     if (baseSet `Set.isSubsetOf` resultSet)
     then
         return "PASSED"
